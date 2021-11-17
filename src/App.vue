471533d9c9ee234c04ac4b379f6baa724fa19626
hyunguk <hyunguk119@gmail.com>
2021-11-17 23:50:10 +0900

    Add new content.

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,94 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
+  <div>
+    <div id="app">
+      <div id="top">
+        <h4>게시판</h4>
+        <p @click="모달창열렸니 = true">게시판 안내</p>
+      </div>
+    </div>
+
+    <div class="black-bg" v-if="모달창열렸니==true">
+      <div class="white-bg">
+        <h4>게시판 사용 주의사항</h4>
+        <p>{{게시판공지사항[0]}} {{게시판공지사항[1]}}</p>
+        <button @click="모달창열렸니 = false"> 닫기 </button>
+      </div>
+    </div>
+
+    <div class="black-bg" v-if="모달창열였다==true">
+      <div class="white-bg">
+        <p v-for="(a,i) in 게시판" :key="i" 누른거 = "i">{{ a.content }}</p>
+        <button @click="모달창열였다 = false"> 닫기 </button>
+      </div>
+    </div>
+    <div class="main">
+      <a v-for="(a,i) in 게시판" :key="i"><a>{{ a.id }}</a> <a>{{ a.writer }}</a> <a @click="모달창열였다 = true" >{{ a.title }}</a> <a>{{ a.date }}</a> <a>{{ a.view }}</a> <a>{{ a.like }}</a> </a>
+    </div>
   </div>
 </template>
 
 <script>
 
+import data from './assets/data';
+
 export default {
   name: 'App',
+  data(){
+    return{
+      조회수 : 0,
+      게시판 : data,
+      좋아요 : 0,
+      누른거 : 0,
+      모달창열렸니 : false,
+      모달창열였다 : false,
+      게시판공지사항 : ['이상한 글 사용 금지','욕 금지'],
+    }
+  },
   components: {
-
-  }
+  },
+  methods : {
+    up(){
+      this.좋아요 += 1;
+    }
+  },
 }
 </script>
 
 <style>
+.main {
+  background: white;
+  padding: 15px;
+  border-radius: 5px;
+}
+.main a {
+  color: black;
+  padding: 10px;
+}
+body {
+  margin : 0;
+}
+div {
+  box-sizing: border-box;
+}
+.black-bg {
+  width: 100%; height:100%;
+  background: rgba(0,0,0,0.5);
+  position: fixed; padding: 20px;
+}
+.white-bg {
+  width: 100%; background: white;
+  border-radius: 8px;
+  padding: 20px;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  margin-top: 60px;
+}
+#top {
   margin-top: 60px;
 }
 </style>
---
+++ src/assets/data.js
@@ -0,0 +1,55 @@
+module.exports=[
+    {
+        id : 0,
+        writer : '홍길동',
+        title: "인생 스토리",
+        content : "하이루",
+        date: "11월 17일",
+        view: "1,000,000",
+        like: 340000
+    },
+    {
+        id : 1,
+        writer : '홍길동',
+        title: "인생 스토리",
+        content : "바이루",
+        date: "11월 17일",
+        view: "1,000,000",
+        like: 340000
+    },
+    {
+        id : 2,
+        writer : '홍길동1',
+        title: "인생 스토리1",
+        content : "스토리",
+        date: "11월 17일",
+        view: "1,000,000",
+        like: 3400000
+    },
+    {
+        id : 3,
+        writer : '홍길동2',
+        title: "인생 스토리2",
+        content : "안녕하세요",
+        date: "11월 17일",
+        view: "1,000,000",
+        like: 34000000
+    },
+    {
+        id : 4,
+        writer : '홍길동3',
+        title: "인생 스토리2",
+        content : "프로젝트",
+        date: "11월 17일",
+        view: "1,000,000",
+        like: 34000000
+    },
+    {
+        id : 5,
+        writer : '홍길동4',
+        title: "인생 스토리4",
+        content : "결혼",
+        date: "11월 17일",
+        view: "1,000,000",
+        like: 34000000
+    }];
